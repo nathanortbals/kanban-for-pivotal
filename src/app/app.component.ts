a1f3a8577e02d368948970fb87d5cf22f66fee9f
Nathan Ortbals <Nathan.Ortbals@veteransunited.com>
2022-07-22 16:32:41 -0500

    Set page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 import { Store } from '@ngrx/store';
 import { map, Observable } from 'rxjs';
+import { selectPivotalProject } from './state/pivotal-project/pivotal-project.selectors';
 import { selectSettings } from './state/settings/settings.selectors';
 
 @Component({
@@ -11,9 +13,15 @@ import { selectSettings } from './state/settings/settings.selectors';
 export class AppComponent {
   showStartPage$: Observable<boolean>;
 
-  constructor(private store: Store) {
+  constructor(private store: Store, private title: Title) {
     this.showStartPage$ = this.store
       .select(selectSettings)
       .pipe(map((settings) => settings === null));
+
+    this.store.select(selectPivotalProject).subscribe((pivotalProject) => {
+      const pageTitle = pivotalProject?.name ?? 'Pivotal For Kanban';
+
+      this.title.setTitle(pageTitle);
+    });
   }
 }
